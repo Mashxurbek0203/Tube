f07f8fb271e7af3a83574e71ed9e26cb982844a1
Mashxurbek <mashxurbekmuhammadjonov@gmail.com>
2021-08-26 17:08:44 +0500

    Bugs related to svg component have been fixed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
 import  Header  from "../../Components/Header/Header"
 import  Main  from "../../Components/Main/Main"
+import Svg from "../../Components/Svgs/Svg"
 function Home() {
   return(
     <>
+    <Svg/>
     <Header/>
     <Main/>
     </>
